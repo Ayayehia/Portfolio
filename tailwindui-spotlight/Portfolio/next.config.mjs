bd8762b1f4e3502d34a86ac4581e3f1e37f13153
ayayehia <ayanader8@gmail.com>
2023-12-14 18:42:58 +0200

    update

--- tailwindui-spotlight/Portfolio/next.config.mjs
+++ tailwindui-spotlight/Portfolio/next.config.mjs
@@ -5,6 +5,9 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 const withMDX = nextMDX({
--- tailwindui-spotlight/Portfolio/src/app/projects/page.jsx
+++ tailwindui-spotlight/Portfolio/src/app/projects/page.jsx
@@ -1,12 +1,6 @@
 import Image from 'next/image'
-
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
 import RealEstateimage from '../../images/logos/Project.jpg'
 import LenskinImage from '../../images/logos/LenSkin.png'
 import AudioStore from '../../images/logos/AudioStore.png'
