25eefdc075cda04f03be6e75e9e5919ba947c488
ayayehia <ayanader8@gmail.com>
2023-12-19 13:31:29 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -123,7 +123,7 @@ function MobileNavigation(props) {
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300 ">
                 <MobileNavItem href="/about">About</MobileNavItem>
-                <MobileNavItem href="/articles">Tech-Stack</MobileNavItem>
+                <MobileNavItem href="/TechStack">Tech-Stack</MobileNavItem>
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
                 <MobileNavItem href="/ContactMe">Contact Me</MobileNavItem>
               </ul>
