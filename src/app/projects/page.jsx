f43649f365dfbcad5d1018ff8dbca366b9d46433
ayayehia <ayanader8@gmail.com>
2025-01-11 21:23:55 +0200

    Update project details and add new Project

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,18 +3,29 @@ import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import RealEstateimage from '../../images/logos/Villa Agency project.png'
 import AudioStore from '../../images/logos/AudioStore.png'
-import Ecommerce from '../../images/logos/EcommerceStore.png'
 import Wickks from '../../images/logos/Wickkslive.png'
+import SingleStop from '../../images/logos/SingleStop.png'
+
 const projects = [
   {
-    name: 'Wickks Platform-(Under Development)',
+    name: 'Wickks Platform',
     description:
       'Collaborated with Observer Agency on an under-development project for Wickks startup. Live preview available.',
     link: { href: 'https://wickkks.vercel.app/', label: 'Live Preview' },
     logo: Wickks,
   },
   {
-    name: 'Real Estate Agency-(Recent)',
+    name: 'Single Stop Project',
+    description:
+      'Collaborated on the development of a user-friendly platform designed to provide access to public benefits and community resources in Usa.',
+    link: {
+      href: 'https://dev.apps.singlestop.org/',
+      label: 'Live Preview',
+    },
+    logo: SingleStop,
+  },
+  {
+    name: 'Real Estate Agency',
     description:
       'Developed a real estate website using React,Tailwind and integrated Google Maps API.',
     link: {
@@ -32,15 +43,6 @@ const projects = [
       label: 'Live Preview',
     },
     logo: AudioStore,
-  },
-  {
-    name: 'Ecommerce-(Oldest Project)',
-    description: 'Working with Real-time data base Firebase and Redux toolkit.',
-    link: {
-      href: 'https://ecommerce-st-eta.vercel.app/',
-      label: 'Live Preview',
-    },
-    logo: Ecommerce,
   },
 ]
 
